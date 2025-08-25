8fa94885c8bd60d543655b2db9adc71ba3da18fb
AbishekSathiyan <abidinesh2213.gmail.com>
2025-08-25 14:10:06 +0530

    Project Added

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { FiHome, FiPlus, FiSearch, FiMessageSquare, FiX } from "react-icons/fi";
 import { auth } from "../config/firebase";
 import { getIdToken } from "firebase/auth";
-import Kamaraj from "../assets/kamarajar (3).jpeg";
+import Kamaraj from "../assets/kamarajar(3).jpeg";
 
 // Central API helper
 const API = {
